6ea7a751c05d2978528201910c94f809007c7388
Yyax13 <138953949+Yyax13@users.noreply.github.com>
2023-08-22 22:26:21 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# SpaartServer
+[![Linguagem](https://img.shields.io/badge/linguagem-Python-blue.svg)](https://www.python.org/)
+[![Licença](https://img.shields.io/badge/licença-CC0-green.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
+![Versão](https://img.shields.io/badge/versão-1.0.0-red.svg)
+
+> Um sistema de criação de servidor para teste de Stress, DoS, DDoS e outros
+
+## Descrição
+
+Este projeto implementa uma maneira de 
+
+## Funcionalidades
+
+- Identificar ip do atacante.
+- Rodar servidores localhost.
+- Volta requests nesse formato:  `<-- | "GET / HTTP/1.1" 200 - | Mozilla/5.0 | 127.0.0.1 | -->`
+
+## Licença
+
+Este projeto está licenciado sob a licença [Creative Commons Zero v1.0 Universal](http://creativecommons.org/publicdomain/zero/1.0/). Essa licença permite que você renuncie ao seu interesse de direitos autorais e dedique seu trabalho ao domínio público. Ela concede permissões para uso comercial, modificação, distribuição e uso privado. A licença isenta garantias, semelhante à Unlicense e a licenças de software típicas.
+
+## Uso
+
+- Clone o repositório.
+- Execute o script Python.
+- Siga as instruções na tela para executar o servidor.
+
+## Contribuições
+
+Contribuições são bem-vindas! Sinta-se à vontade para fazer um fork e enviar pull requests.
+
+## Contato
+
+Para perguntas ou sugestões, você pode me encontrar no Discord:
+> solo.yyax
+
+Ou no meu e-mail:
+> Yyax13@proton.me
+
+---
+© Lucas de Moraes "Yyax" Claro, 2023
---
+++ main.py
@@ -0,0 +1,39 @@
+import http.server
+import socketserver
+import random
+from libs.color import *
+
+tagn1 = f"{c.cyan('[')}{c.yellow('1')}{c.cyan(']')}"
+tagn2 = f"{c.cyan('[')}{c.yellow('2')}{c.cyan(']')}"
+hashtag = f"{c.cyan('[')}{c.yellow('#')}{c.cyan(']')}"
+
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        request_info = f"{c.yellow('<--')} {c.yellow('|')} {c.cyan('Request Code')} {c.yellow('|')} {c.purple('IP')} {c.yellow('|')} {c.yellow('-->')}"
+        
+        request_line = f"{c.yellow('<--')} | {c.cyan(format % args)} | {c.green(self.headers.get('User-Agent', 'Unknown').split('(')[0].strip())} | {c.purple(self.client_address[0])} | {c.yellow('-->')}"
+        print(request_line)
+
+
+port_choose = input(f"""
+    {tagn1} {c.cyan('Random Port')}
+    {tagn2} {c.cyan('Choose Port')}
+
+    {hashtag} {c.cyan('Choose one')}: """)
+
+if port_choose == "1":
+    port = random.randint(1024, 80000)
+elif port_choose == "2":
+    port = int(input(f'    {hashtag} {c.cyan("Choose a port (more than 1024): ")}'))
+    if port <= 1024:
+        print(f"{c.red('Invalid port number!')} Please choose a port greater than 1024.")
+        exit(1)
+else:
+    print(f"{c.red('Invalid choice!')} Exiting...")
+    exit(1)
+
+handler = CustomHTTPRequestHandler 
+
+with socketserver.TCPServer(("", port), handler) as httpd:
+    print(f"Servidor rodando em http://localhost:{port}")
+    httpd.serve_forever()
